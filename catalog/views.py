ed17b20ac9d1973b6c0c685ed22bf0a96136ebce
Kirill Drago <dragoswim7@gmail.com>
2022-11-09 00:32:27 +0200

    create temporary templates

--- catalog/views.py
+++ catalog/views.py
@@ -45,4 +45,4 @@ class RedactorListView(generic.ListView):
 
 class RedactorDetailView(generic.DetailView):
     model = Redactor
-    queryset = Redactor.objects.prefetch_related("newspapers")
+    # queryset = Redactor.objects.prefetch_related("newspapers")
