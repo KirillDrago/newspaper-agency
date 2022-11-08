6c79dcafca7b5d894e39eb8f023cf5edbab103ce
Kirill Drago <dragoswim7@gmail.com>
2022-11-08 17:23:22 +0200

    Create models and connect app to the settings

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField
+    published_date = models.DateTimeField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
