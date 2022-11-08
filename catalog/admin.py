4d237df6820d314d8f4ff02dfa82a7e722fe1878
Kirill Drago <dragoswim7@gmail.com>
2022-11-08 17:32:18 +0200

    Create admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from catalog.models import Topic, Newspaper, Redactor
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ["title", "publishers", "published_date"]
+    search_fields = ["title"]
+    list_filter = ["topic"]
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
